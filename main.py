b6d53e912fef7d67dcf79570863bac700e84e989
GREED <43282278+Kutcher1945@users.noreply.github.com>
2023-01-11 17:02:39 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "ENTER YOUR API KEY HERE"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
@@ -15,7 +15,7 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "ENTER YOUR TELEGRAM TOKEN HERE"
+    tgkey = "5476324482:AAG9EobcWDxyXW5PQB6t0q2Na-Sn_bycXvc"
 else:
     tgkey = tgenv
 print(tgenv)
